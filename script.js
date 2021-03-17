a7bac1b1fa7c6e4e5f0bbcc9baab7f7a196696ad
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-17 16:25:56 +0100

    nome e cognome stampati

--- script.js
+++ script.js
@@ -8,6 +8,41 @@ var lista = document.getElementById("student_data");
 
 for (const key in studente) {
     
-    lista.innerHTML += "<li>" + key + " " + studente[key] + "</li>";
+    lista.innerHTML += "<li>" + key + ":" + " " + studente[key] + "</li>";
+
+}
+
+var listaStudenti = document.getElementById("student_list");
+var allStudenti = [
+    {
+        cognome: "Giordano",
+        nome: "Antonio",
+        eta: 21
+    },
+    {
+        cognome: "Conti",
+        nome: "Sabrina",
+        eta: 21
+    },
+    {
+        cognome: "Alfonsi",
+        nome: "Andrea",
+        eta: 21
+    },
+    {
+        cognome: "Rigoldi",
+        nome: "Carolina",
+        eta: 21
+    },
+    {
+        cognome: "Varisco",
+        nome: "Fabio",
+        eta: 21
+    }
+];
+
+for (let i = 0; i < allStudenti.length; i++) {
+    
+    listaStudenti.innerHTML += "<li>" + allStudenti[i]["nome"] + " " +allStudenti[i]["cognome"] + "</li>";
 
 }
